2b88f1078c5336e1d5fd5c8b0231af4e448b4e57
Andrey Tulush <tulushandrey200505@gmail.com>
2023-08-03 17:47:41 +0300

    admin-panel

--- first_app/admin.py
+++ first_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from .models import Advertisement
+
 # Register your models here.
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'price', 'created_at', 'update_at', 'auction']
+    list_filter = ['auction']
+
+
+admin.site.register(Advertisement, AdvertisementAdmin)
+
--- first_app/models.py
+++ first_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+from django.utils import timezone
+from django.utils.html import format_html
 
 # Create your models here.
 
@@ -12,7 +15,26 @@ class Advertisement(models.Model):
     updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"title={self.title}, description={self.description}, price={self.price}, auction={self.auction}"
+        return f"Advertisement(title={self.title}, description={self.description}, price={self.price}, auction={self.auction})"
 
+    @admin.display(description='Дата создания')
+    def created_at(self):
+        if self.created_date.date() == timezone.now().date():
+            created_time = self.created_date.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+
+        return self.created_date.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='Дата обновления')
+    def update_at(self):
+        if self.updated_date.date() == timezone.now().date():
+            created_time = self.updated_date.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: yellow; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+
+        return self.updated_date.strftime("%d.%m.%Y в %H:%M:%S")
     class Meta:
         db_table = 'Advertisement'
