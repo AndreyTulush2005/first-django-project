84c546425d0d08662db068ccc2ec61f97c3432e4
Andrey Tulush <tulushandrey200505@gmail.com>
2023-08-22 18:41:38 +0300

    forms

--- first_app/urls.py
+++ first_app/urls.py
@@ -3,11 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-from .views import index, top_sellers
+from .views import index, top_sellers, advertisement_post
 
 urlpatterns = [
     path('', index, name='main-page'),
     path('top-sellers', top_sellers, name='top-sellers'),
+    path('advertisement-post', advertisement_post, name='adv_post')
 ]
 
 if settings.DEBUG:
--- first_app/views.py
+++ first_app/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from .forms import AdvertisementForm
+
 from .models import Advertisement
+
+
 
 def index(request):
     advertisements = Advertisement.objects.all()
@@ -10,3 +16,21 @@ def index(request):
 
 def top_sellers(request):
     return render(request=request, template_name='top-sellers.html')
+
+def advertisement_post(request):
+    if request.method == "POST":
+        form = AdvertisementForm(request.POST, request.FILES)
+        if form.is_valid():
+            advertisement = Advertisement(**form.cleaned_data)
+            advertisement.user = request.user
+            advertisement.save()
+            url = reverse('main-page')
+
+            return redirect(url)
+    else:
+        form = AdvertisementForm()
+
+    context = {
+        'form': form
+    }
+    return render(request=request, template_name='advertisement-post.html', context=context)
